01315ea9f3037d73554052af7c7ce16889e5a8f8
Yash Saxena <62505279+yashsaxena17@users.noreply.github.com>
2021-09-18 00:09:35 +0530

    Add files via upload

---
+++ CountSquares.py
@@ -0,0 +1,11 @@
+def countSquares(self, N):
+    count = 0
+    for i in range(1, N):
+        if i**2 < N:
+            count+=1
+        else:
+            break
+    return count
+n = int(input())
+ans = countSquares(n)
+print(ans)
